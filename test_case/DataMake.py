9afc1161c7b1231a728222aa075f03d1e4889c65
Su Liang <suliangxd@gmail.com>
2016-03-10 12:34:41 +0800

    modified DataMake.py to mkdir file

--- test_case/DataMake.py
+++ test_case/DataMake.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import random
+import random, os
 
 def rand_int(a, b):
     '''
@@ -57,3 +57,48 @@ if __name__ == '__main__':
     write_str += '\n'
     file_handle.write(write_str)
     file_handle.close()
+
+        # 生成计算数据
+    file_handle = open(file_solve, 'w')
+    start_id = rand_int(0, point_num - 1)
+    end_id = rand_int(0, point_num - 1)
+    while start_id == end_id:
+        end_id = rand_int(0, point_num - 1)
+    including_set = []
+    while len(including_set) <= point_v_num:
+        tmp = rand_int(0, point_num - 1)
+        if tmp not in including_set:
+            including_set.append(tmp)
+    write_str = str(start_id) + ',' + str(end_id) + ','
+    First = True
+    for i in including_set:
+        if First:
+            write_str += str(i)
+            First = False
+        else:
+            write_str += '|'+str(i)
+    write_str += '\n'
+    file_handle.write(write_str)
+    file_handle.close()
+
+    # 测试数据组织到一个文件夹里
+    if os.path.exists('./CASE_VERSION'):
+        file_handle = open('CASE_VERSION', 'r')
+        case_version = file_handle.read()
+        if case_version == None:
+            case_version = 0
+            os.system('rm -rf case*')
+        case_version = filter(str.isdigit, case_version)
+        case_version = int(case_version) + 1
+        file_handle.close()
+        file_handle = open('CASE_VERSION', 'w+')
+        file_handle.write(str(case_version))
+        file_handle.close()
+        case_file = 'case'+ str(case_version)
+        if 0 == os.system('mkdir %s' % case_file):
+            os.system('mv %s %s' % (file_graph, case_file))
+            os.system('mv %s %s' % (file_solve, case_file))
+        else:
+            raise IOError('mkdir casefile Error!!!')
+    else:
+        raise IOError('case_version not exist!!!')
